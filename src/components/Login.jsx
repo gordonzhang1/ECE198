2b6d10f8526a9dad3f805676b392cbab6469b2f0
Gordon06060 <gordonz0913@gmail.com>
2024-11-05 14:59:10 -0500

    pasted login page into mainjsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ export default function Login() {
   return (
     <section className="vh-100">
       <div className="container-fluid h-custom">
-        <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="outercontainer-2">
           <div className="col-md-9 col-lg-6 col-xl-5">
             <img
               src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
@@ -95,15 +95,13 @@ export default function Login() {
                     Register
                   </a>
                 </p>
+                <p>Continue as guest</p>
               </div>
             </form>
           </div>
         </div>
       </div>
       <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-        <div className="text-white mb-3 mb-md-0">
-          Copyright © 2020. All rights reserved.
-        </div>
         <div>
           <a href="#!" className="text-white me-4">
             <i className="fab fa-facebook-f"></i>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import Login from "./components/Login";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Login />
   </StrictMode>
 );
