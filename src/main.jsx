89c52f4d9676e201e348d13277814a179585fad3
Gordon06060 <gordonz0913@gmail.com>
2024-11-05 15:20:23 -0500

    Implemented Router and login page

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import Login from "./components/Login";
+import Index from "./Index";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Login />
+    <Index />
   </StrictMode>
 );
